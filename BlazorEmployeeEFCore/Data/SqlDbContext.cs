cd0c426c37ba38a6399eb8f5aaa65b353a7f1906
damilola <damee1993@gmail.com>
2019-11-17 17:09:21 +0100

    Add project files.

---
+++ BlazorEmployeeEFCore/Data/SqlDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorEmployeeEFCore.Data
+{
+    public class SqlDbContext : DbContext
+    {
+        public SqlDbContext(DbContextOptions<SqlDbContext> options)
+           : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
---
+++ BlazorEmployeeEFCore/readme.md
@@ -0,0 +1,3 @@
+﻿#Blazor application
+
+1) this is my first project on blazor, I intend to build a robust hr solution piece-by-piece
